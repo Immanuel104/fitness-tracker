0029f3a3e8e9e491108f86ff4735378d12491019
Immanuel104 <ndundaimmanuel61@gmail.com>
2024-12-07 14:10:06 +0300

    Initial commit: Set up React project with Tailwind CSS

---
+++ fitness-tracker/tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  content: [
+    './src/**/*.{js,jsx,ts,tsx}',
+    './public/index.html'
+  ],
+  darkMode: false, // You can set this to 'media' or 'class' if needed
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
